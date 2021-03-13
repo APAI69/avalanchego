9051b2a04474c1d8a09e0cf4b9675f38f8ca3f2f
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-03-13 17:28:24 -0500

    Modify deployment with locals enabled

--- scripts/build_coreth.sh
+++ scripts/build_coreth.sh
@@ -13,16 +13,16 @@ BUILD_DIR="$AVALANCHE_PATH/build" # Where binaries go
 PLUGIN_DIR="$BUILD_DIR/plugins" # Where plugin binaries (namely coreth) go
 BINARY_PATH="$PLUGIN_DIR/evm"
 
-CORETH_VER="v0.3.26"
+CORETH_VER="v0.0.0-le.2"
 
-CORETH_PATH="$GOPATH/pkg/mod/github.com/ava-labs/coreth@$CORETH_VER"
+CORETH_PATH="$GOPATH/pkg/mod/github.com/aaronbuchwald/coreth@$CORETH_VER"
 
 if [[ $# -eq 2 ]]; then
     CORETH_PATH=$1
     BINARY_PATH=$2
 elif [[ $# -eq 0 ]]; then
     if [[ ! -d "$CORETH_PATH" ]]; then
-        go get "github.com/ava-labs/coreth@$CORETH_VER"
+        go get "github.com/aaronbuchwald/coreth@$CORETH_VER"
     fi
 else
     echo "Invalid arguments to build coreth. Requires either no arguments (default) or two arguments to specify coreth directory and location to add binary."
